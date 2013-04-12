ff07e7ba1269ee4db815a08d79d2626f50e79868
Michael Pillsbury <michael.pillsbury@tstmedia.com>
2013-04-12 09:46:48 -0500

    new empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loanstar::Application.config.secret_token = 'a2cfd4170cc2d2dda3671c3fd9eb86ba33e1a419cf31cb9617daa6da1f36a501511eeed6ad39d5c0cf74393a3aebc1c35eafa07208245496a226492b0bee0463'
