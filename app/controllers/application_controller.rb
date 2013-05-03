67b979de7b75b90adac8d570dbbed3118718ab62
Michael Pillsbury <icylucifer@gmail.com>
2013-05-02 20:31:22 -0500

    enable cors for Chad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_filter :cors
+
+  def cors
+    headers["Access-Control-Allow-Origin"] = "js-app-origin.com"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
   def failure message
-      render json: {
-        status: "failure",
-        message: message
-      }
+    render json: {
+      status: "failure",
+      message: message
+    }
   end
 end
